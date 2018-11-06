297d4f38e5842f8ae036715e8124d0534f4b0776
Vinicius Lemos <vlemos@gmail.com>
2018-11-05 23:01:43 -0200

    Criando Repository e Service para Categoria

--- src/main/java/com/vlemos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vlemos/cursomc/resources/CategoriaResource.java
@@ -6,8 +6,12 @@
 package com.vlemos.cursomc.resources;
 
 import com.vlemos.cursomc.domain.Categoria;
+import com.vlemos.cursomc.services.CategoriaService;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,17 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
     
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar(){
-        Categoria cat1 = new Categoria (1,"Informatica");
-        Categoria cat2 = new Categoria (2,"Escritorio");
+    @Autowired
+    private CategoriaService service;
+    
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id){
         
-        List<Categoria> lista = new ArrayList();
-        lista.add(cat1);
-        lista.add(cat2);
-        
-        
-        return lista;
+        Categoria obj = service.buscar(id);
+
+        return ResponseEntity.ok().body(obj);
     }
     
 }
---
+++ src/main/java/com/vlemos/cursomc/services/CategoriaService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vlemos.cursomc.services;
+
+import com.vlemos.cursomc.domain.Categoria;
+import com.vlemos.cursomc.repositories.CategoriaRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Vinicius Lemos
+ */
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+    
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj;
+        obj = repo.findById(id);
+        return obj.orElse(null);
+        
+    }
+
+}
