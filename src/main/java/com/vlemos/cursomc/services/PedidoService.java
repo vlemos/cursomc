17fabf33eddffdc0fdcbf99f344023434cb87631
Vinicius Lemos <vlemos@gmail.com>
2018-11-12 17:13:09 -0200

    Atualizando categoria com Put, outros pequenos ajustes

--- src/main/java/com/vlemos/cursomc/services/PedidoService.java
+++ src/main/java/com/vlemos/cursomc/services/PedidoService.java
@@ -23,7 +23,7 @@ public class PedidoService {
     @Autowired
     private PedidoRepository repo;
     
-    public Pedido buscar(Integer id) {
+    public Pedido find(Integer id) {
         Optional<Pedido> obj;
         obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
