2f011fcb82c2c60a494bbaa9ae0c751b752f2960
Vinicius Lemos <vlemos@gmail.com>
2018-11-13 18:12:03 -0200

    Paginação com Parametros Opcionais na requisição

--- src/main/java/com/vlemos/cursomc/services/CategoriaService.java
+++ src/main/java/com/vlemos/cursomc/services/CategoriaService.java
@@ -14,6 +14,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 /**
@@ -55,6 +58,14 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return repo.findAll();
+    }
+    
+    /*
+    Retorna todos com paginação
+    */
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage,Sort.Direction.valueOf(direction),orderBy);
+        return repo.findAll(pageRequest);
     }
 
 }
