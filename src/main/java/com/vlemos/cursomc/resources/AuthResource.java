0ffd510b699b16a08bdefd18ab7a8c6f55a30fb0
Vinicius Lemos <vlemos@gmail.com>
2019-02-26 13:55:44 -0300

    refresh token

---
+++ src/main/java/com/vlemos/cursomc/resources/AuthResource.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vlemos.cursomc.resources;
+
+/**
+ *
+ * @author vinicius.lemos
+ */
+public class AuthResource {
+    
+}
